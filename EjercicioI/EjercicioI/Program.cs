69f722ba53d9515bf80d12b45271ac19885cf0a6
JoseLlampa <62297243+JoseLlampa@users.noreply.github.com>
2020-03-22 14:16:22 -0300

    Contador del 1 al 321

---
+++ EjercicioI/EjercicioI/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int respuesta = 1;
+
+            while (respuesta < 321)
+            {
+                Console.WriteLine(respuesta);
+                int cont = respuesta + 1;
+                respuesta = cont;
+            }
+        }
+    }
+}
